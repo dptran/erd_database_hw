6019d293d840f6879eac036b56b6d52628107457
dptran <darren-tran@hotmail.com>
2021-11-18 13:53:18 -0800

    final commit

--- Movie Theater Database_postgres_create.sql
+++ Movie Theater Database_postgres_create.sql
@@ -2,7 +2,7 @@ CREATE TABLE "public.Customers" (
 	"id" serial,
 	"first_name" VARCHAR(255) NOT NULL,
 	"last_name" VARCHAR(255) NOT NULL,
-	"created_at" DATETIME,
+	"created_at" date,
 	"concession_id" integer NOT NULL,
 	"ticket_id" VARCHAR(255) NOT NULL,
 	CONSTRAINT "Customers_pk" PRIMARY KEY ("id")
@@ -27,8 +27,8 @@ CREATE TABLE "public.Tickets" (
 CREATE TABLE "public.Concessions" (
 	"id" serial,
 	"name" VARCHAR(255),
-	"created_at" DATETIME NOT NULL,
-	"updated_at" DATETIME NOT NULL,
+	"created_at" date NOT NULL,
+	"updated_at" date NOT NULL,
 	"customer_id" integer NOT NULL,
 	"purchases" FLOAT NOT NULL,
 	CONSTRAINT "Concessions_pk" PRIMARY KEY ("id")
@@ -50,15 +50,15 @@ CREATE TABLE "public.Movies" (
 
 
 
-ALTER TABLE "Customers" ADD CONSTRAINT "Customers_fk0" FOREIGN KEY ("concession_id") REFERENCES "Concessions"("id");
-ALTER TABLE "Customers" ADD CONSTRAINT "Customers_fk1" FOREIGN KEY ("ticket_id") REFERENCES "Tickets"("id");
+ALTER TABLE "public.Customers" ADD CONSTRAINT "Customers_fk0" FOREIGN KEY ("concession_id") REFERENCES "public.Concessions"("id");
+ALTER TABLE "public.Customers" ADD CONSTRAINT "Customers_fk1" FOREIGN KEY ("ticket_id") REFERENCES "public.Tickets"("id");
 
-ALTER TABLE "Tickets" ADD CONSTRAINT "Tickets_fk0" FOREIGN KEY ("customer_id") REFERENCES "Customers"("id");
-ALTER TABLE "Tickets" ADD CONSTRAINT "Tickets_fk1" FOREIGN KEY ("movie_id") REFERENCES "Movies"("id");
+ALTER TABLE "public.Tickets" ADD CONSTRAINT "Tickets_fk0" FOREIGN KEY ("customer_id") REFERENCES "public.Customers"("id");
+ALTER TABLE "public.Tickets" ADD CONSTRAINT "Tickets_fk1" FOREIGN KEY ("movie_id") REFERENCES "public.Movies"("id");
 
-ALTER TABLE "Concessions" ADD CONSTRAINT "Concessions_fk0" FOREIGN KEY ("customer_id") REFERENCES "Customers"("id");
+ALTER TABLE "public.Concessions" ADD CONSTRAINT "Concessions_fk0" FOREIGN KEY ("customer_id") REFERENCES "public.Customers"("id");
 
-ALTER TABLE "Movies" ADD CONSTRAINT "Movies_fk0" FOREIGN KEY ("customer_id") REFERENCES "Customers"("id");
+ALTER TABLE "public.Movies" ADD CONSTRAINT "Movies_fk0" FOREIGN KEY ("customer_id") REFERENCES "public.Customers"("id");
 
 
 
